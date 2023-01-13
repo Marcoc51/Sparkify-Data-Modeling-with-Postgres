5a6ad128dd1ad214692148ff5dd9a318d62639d3
Marc Sanad <64839030+Marcoc51@users.noreply.github.com>
2023-01-13 12:47:19 +0200

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -7,32 +7,59 @@ artist_table_drop = "drop table if exists artists;"
 time_table_drop = "drop table if exists time;"
 
 # CREATE TABLES
-
-songplay_table_create = ("""create table if not exists songplays(
-                            start_time bigint, user_id int not null, level varchar not null, song_id varchar, artist_id varchar, 
-                            session_id int not null, location varchar, user_agent varchar not null,
-                            primary key(session_id, start_time)
-                            );
+user_table_create = ("""
+create table if not exists users(
+user_id int primary key,
+first_name varchar,
+last_name varchar,
+gender varchar,
+level varchar
+);
 """)
 
-user_table_create = ("""create table if not exists users(
-                            user_id int primary key, first_name varchar, last_name varchar, gender varchar, level varchar
-                            );
+song_table_create = ("""
+create table if not exists songs(
+song_id varchar primary key,
+title varchar not null,
+artist_id varchar,
+year int,
+duration numeric not null
+);
 """)
 
-song_table_create = ("""create table if not exists songs(
-                            song_id varchar primary key, title varchar not null, artist_id varchar, year int, duration numeric not null
-                            );
+artist_table_create = ("""
+create table if not exists artists(
+artist_id varchar primary key,
+name varchar not null,
+location varchar,
+latitude text,
+longitude text
+);
 """)
 
-artist_table_create = ("""create table if not exists artists(
-                            artist_id varchar primary key, name varchar not null, location varchar, latitude text, longitude text
-                            );
+time_table_create = ("""
+create table if not exists time(
+start_time time unique,
+hour int,
+day int,
+week int,
+month int,
+year int,
+weekday int
+);
 """)
 
-time_table_create = ("""create table if not exists time(
-                            start_time varchar, hour int, day int, week int, month int, year int, weekday int
-                            );
+songplay_table_create = ("""
+create table if not exists songplays(
+songplay_id SERIAL primary key, 
+start_time time references time(start_time), 
+user_id int not null references users(user_id),
+level varchar not null,
+song_id varchar references songs(song_id), 
+artist_id varchar references artists(artist_id), 
+session_id int not null,
+location varchar,
+user_agent varchar not null);
 """)
 
 # INSERT RECORDS
@@ -46,7 +73,7 @@ songplay_table_insert = ("""insert into songplays(
 user_table_insert = ("""insert into users(
                             user_id, first_name, last_name, gender, level
                             ) VALUES (%s, %s, %s, %s, %s)
-                            ON CONFLICT DO NOTHING;
+                            ON CONFLICT(user_id) DO UPDATE SET level = excluded.level;
 """)
 
 song_table_insert = ("""insert into songs(
@@ -77,5 +104,5 @@ song_select = ("""select s.song_id, a.artist_id\
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
